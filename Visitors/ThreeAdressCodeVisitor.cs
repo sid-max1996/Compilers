6f3fb5893d42c3d91ae026c7b380e1db679499ef
Max <35890812+sid-max1996@users.noreply.github.com>
2019-03-14 21:30:25 +0300

    Init Project

---
+++ Visitors/ThreeAdressCodeVisitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProgramTree;
+
+namespace SimpleLang.Visitors
+{
+    class ThreeAdressCodeVisitor: AutoVisitor
+    {
+        public override void VisitIfNode(IfNode c)
+        {
+            
+        }
+    }
+}
