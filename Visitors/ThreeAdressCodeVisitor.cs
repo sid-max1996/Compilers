7ca165d46c3676eb1818e23fb7474716399103c0
Mike <ams2501@mail.ru>
2019-03-20 13:27:13 +0300

    Start 3 adr code

--- Visitors/ThreeAdressCodeVisitor.cs
+++ Visitors/ThreeAdressCodeVisitor.cs
@@ -6,11 +6,30 @@ using ProgramTree;
 
 namespace SimpleLang.Visitors
 {
-    class ThreeAdressCodeVisitor: AutoVisitor
+    class ThreeAdressCodeVisitor : AutoVisitor
     {
-        public override void VisitIfNode(IfNode c)
+        private LinkedList<Five> fives = new LinkedList<Five>;
+        private int tmpcount = 0;
+        private int lblcount = 0;
+        public override void VisitAssignNode(AssignNode a)
         {
             
+            new Five { Op = "-", Arg1 = a.Expr, Arg2 = a.Expr, Res = a.Id };
+            new Five { Op = "+", Arg1 = a.Expr, Arg2 = a.Expr, Res = a.Id };
         }
+     
+        private Five GenAssignFive(ExprNode expr, ExprNode id) => new Five { Op = "=", Arg1 = expr, Res = id };
+        private Five GenIdMinusFive(ExprNode expr, ExprNode id) => new Five { Op = "-", Arg1 = expr, Res = id };
+        private Five GenIdMinusFive(ExprNode expr1, ExprNode expr2, ExprNode id) => new Five { Op = "-", Arg1 = expr1, Arg2 = expr2, Res = id };
+        private string genTmpName() => $"t{tmpcount++}";
+        private string genLblName() => $"L{lblcount++}";
+
+
     }
+    class Five
+    {
+        public ExprNode Label, Arg1, Arg2, Res;
+        public string Op;
+    }
+
 }
